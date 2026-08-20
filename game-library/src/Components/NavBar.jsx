e79f31414c93203d1b6babf40fb2712b7e1d70d3
TheBaNoNa123 <rohan.singh42351@gmail.com>
2026-08-20 14:21:11 -0700

    Added profile logo / Styled background

--- game-library/src/Components/NavBar.jsx
+++ game-library/src/Components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import profile_logo from '../assets/profile_logo.svg'
 
 const NavBar = () => {
   return (
     <header>
         <nav className="navBar">
           <button className="navBut navBut-Mid">Search</button>
-          <button className="navBut navBut-Right">Profile</button>
+          <button className="navProfileBut">
+            <img src={profile_logo} alt="profile_logo" className="profileLogo" />
+          </button>
         </nav>
     </header>
   )
