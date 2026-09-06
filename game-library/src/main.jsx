d422bd76b0861ff16fcfebd6afbe52668822d2bd
Rohan <rohan.singh42351@gmail.com>
2026-09-06 16:25:58 -0700

    Adding routes

--- game-library/src/main.jsx
+++ game-library/src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import './index.css'
 import App from './App.jsx'
+import Register from './Components/Register.jsx';
+import Login from './Components/Login.jsx';
+
+
+const router = createBrowserRouter([
+    {path: "/", element: <App />},
+    {path: "/Register", element: <Register />},
+    {path: "/Login", element: <Login />}
+
+])
 
 createRoot(document.getElementById('root')).render(
 
-    <App />
+    <RouterProvider router={router} />
   
 )
