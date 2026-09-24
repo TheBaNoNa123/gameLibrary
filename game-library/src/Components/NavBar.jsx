b4a2835da5b7e5ba0fd511bcf0eb4a6c3dca7333
Rohan <rohan.singh42351@gmail.com>
2026-09-24 13:26:06 -0700

    Profile button links to user's profile

--- game-library/src/Components/NavBar.jsx
+++ game-library/src/Components/NavBar.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import profile_logo from '../assets/profile_logo.svg'
+import { useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
+  const navigate = useNavigate();
+
+  const goToProfile = () => {
+    navigate("/Profile")
+  }
   return (
     <header>
         <nav className="navBar">
           <button className="navBut navBut-Mid">Search</button>
-          <button className="navProfileBut">
+          <button className="navProfileBut" onClick={goToProfile}>
             <img src={profile_logo} alt="profile_logo" className="profileLogo" />
           </button>
         </nav>
